5d99bf02d312202cd2eb6ce631e398980709008a
unknown <kairan95.zhai@gmail.com>
2024-10-20 16:52:26 -0400

    implement setCurrentUser method

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set current user.
+     * @param name the current user who logged in.
+     */
+    void setCurrentUser(String name);
 }
